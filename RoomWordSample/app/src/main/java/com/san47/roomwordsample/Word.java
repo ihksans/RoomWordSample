54b2fa4219109e3391bc1c5f65247a862d523df9
ikhsan.setiawan.tif18 <ikhsan.setiawan.tif18@polban.ac.id>
2020-03-16 12:51:32 +0700

    latihan sesuai prosedure

---
+++ RoomWordSample/app/src/main/java/com/san47/roomwordsample/Word.java
@@ -0,0 +1,26 @@
+package com.san47.roomwordsample;
+
+import androidx.annotation.NonNull;
+import androidx.lifecycle.LiveData;
+import androidx.room.ColumnInfo;
+import androidx.room.Dao;
+import androidx.room.Entity;
+import androidx.room.Insert;
+import androidx.room.OnConflictStrategy;
+import androidx.room.PrimaryKey;
+import androidx.room.Query;
+
+import java.util.List;
+
+@Entity(tableName = "word_table")
+public class Word {
+    @PrimaryKey
+    @NonNull
+    @ColumnInfo(name = "word")
+    private String mWord;
+
+    public Word(@NonNull String word) {this.mWord = word;}
+
+    public String getWord(){return this.mWord;}
+}
+
